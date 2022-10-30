f07473630f13366c136bed32682ab1aac63725f1
ioanapanc <panc.family@yahoo.com>
2022-10-30 19:26:08 +0200

    add project to github

---
+++ weather-app-main/api/endpoints.js
@@ -0,0 +1,11 @@
+
+const API_KEY = "4e5953d7985f127ab19e74667073cf01";
+
+
+function getCurrentWeatherEndpoint(city) {
+  return `https://api.openweathermap.org/data/2.5/weather?q=${city}&lang=ro&units=metric&appid=${API_KEY}`;
+}
+
+function getForecastEndpoint(city) {
+  return `https://api.openweathermap.org/data/2.5/forecast?q=${city}&lang=ro&units=metric&appid=${API_KEY}`;
+}
